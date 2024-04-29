110c7a78d00ff8e6a789253d49adb382a93823aa
ManseurNassim <nassimmanseur1@gmail.com>
2024-04-29 02:07:10 +0200

    refonte du portfolio

--- script.js
+++ script.js
@@ -1,3 +1,48 @@
+let Menu = document.getElementById("menuimg");
+let menuElements = document.querySelectorAll('#menu li');
+let fullMenu = document.querySelector("#Accueil nav");
+let Menuzone = document.getElementById("menu");
+function changerCSS() {
+    var cssLink = document.getElementById("css-link");
+    if (cssLink.href.includes("styles.css")) {
+        cssLink.href = "styles1.css";
+    } else {
+        cssLink.href = "styles.css";
+    }
+}
+
+// Ajoute un gestionnaire d'événements pour le survol de la souris sur le bouton Menu
+Menu.addEventListener('mouseenter', function() {
+    // Modifier la largeur de la Menuzone
+    Menuzone.style.transition = "width 0.5s ease"; // Ajoute une transition
+    Menuzone.style.width = "620px"; // Ajuste la largeur à 100%
+});
+
+// Ajoute un gestionnaire d'événements pour le départ de la souris du bouton Menu
+Menuzone.addEventListener('mouseleave', function() {
+    // Réinitialise la largeur de la Menuzone
+    Menuzone.style.transition = "width 0.5s ease"; // Ajoute une transition
+    Menuzone.style.width = "8%"; // Ajuste la largeur à sa valeur originale
+});
+
+// Parcourir tous les éléments de la liste, sauf le dernier
+for (let i = 0; i < menuElements.length - 1; i++) {
+    let element = menuElements[i];
+    // Ajoute un gestionnaire d'événements pour le survol de la souris sur l'élément Menu
+    Menu.addEventListener('mouseenter', function() {
+        // Modifier l'opacité de l'élément parcouru actuellement
+        element.style.opacity = '1';
+    });
+    // Ajoute un gestionnaire d'événements pour le départ de la souris de l'élément Menu
+    fullMenu.addEventListener('mouseleave', function() {
+        // Réinitialiser l'opacité de l'élément parcouru actuellement à 0
+        if (element !== Menu) {
+            element.style.opacity = '0';
+        }
+    });
+}
+
+
 var isNavVisible = true; // Variable pour suivre l'état de la barre de navigation
 
 // Options de l'Intersection Observer
@@ -62,28 +107,29 @@ function maFonctionDeRetablissement() {
 document.addEventListener("DOMContentLoaded", function () {
     const darkModeButton = document.getElementById('darkmode');
     let isDarkMode = false; // Initialisation à "Jour"
-    const containerElements = document.getElementsByClassName('container1');
-        darkModeButton.addEventListener('click', () => {
+
+    darkModeButton.addEventListener('click', () => {
         if (isDarkMode) {
             // Passer en mode "Jour"
             darkModeButton.style.backgroundImage = "url('img/darkmode.png')";
-            document.body.style.backgroundColor = '#c8c0fc'; // Couleur de fond du corps en mode jour
-            for (let i = 0; i < containerElements.length; i++) {
-                containerElements[i].style.backgroundColor = '#ffffff'; // Remplacez par la couleur de votre choix
-            }
+            document.getElementById('roquette').src = "img/darkroquette.png"; // Changer l'image de la roquette en mode jour
+            document.documentElement.style.setProperty('--bg-color', '#FCFDF8');
+            document.documentElement.style.setProperty('--secondary-color', 'black');
+            document.querySelector('header').style.backgroundImage = "url('img/day_background.jpg')"; // Changer l'image de fond du header en mode jour
 
             isDarkMode = false;
         } else {
             // Passer en mode "Nuit"
-            isDarkMode = true;
             darkModeButton.style.backgroundImage = "url('img/lightmode.png')";
-            document.body.style.backgroundColor = '#38306b'; // Couleur de fond du corps en mode nuit
-            for (let i = 0; i < containerElements.length; i++) {
-                containerElements[i].style.backgroundColor = '#a5a4a4'; // Remplacez par la couleur de votre choix
-            }
+            document.getElementById('roquette').src = "img/roquette.png"; // Changer l'image de la roquette en mode nuit
+            document.documentElement.style.setProperty('--bg-color', '#211566'); // Couleur de fond du corps en mode nuit
+            document.documentElement.style.setProperty('--secondary-color', '#FCFDF8');
+            document.querySelector('header').style.backgroundImage = "url('img/space.png')"; // Changer l'image de fond du header en mode nuit
 
-        };
+            isDarkMode = true;
+        }
     });
 });
+
 
 
